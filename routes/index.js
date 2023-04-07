05d689fe1da607b922c68bfc851e7e474fe9dfcc
Lempereur <lempereur.julien83@gmail.com>
2023-04-07 10:46:28 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var router = express.Router();
+const fetch = require('node-fetch');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/year', (req, res)=>{
+  const date = new Date();
+  res.json({date : date.getFullYear()})
+})
+
+module.exports = router;
